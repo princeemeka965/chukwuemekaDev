3f8989930470af1d930427b7b16c9cdfbcaddfe1
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2026-09-05 00:09:50 +0100

    Update WhatsApp number in utility function for link generation

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,4 +1,4 @@
-const WHATSAPP_NUMBER = "2349028098699";
+const WHATSAPP_NUMBER = "2348163276826";
 
 export function getWhatsAppLink(message = "Hi Chukwuemeka, I'd like to talk about a project.") {
     return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
